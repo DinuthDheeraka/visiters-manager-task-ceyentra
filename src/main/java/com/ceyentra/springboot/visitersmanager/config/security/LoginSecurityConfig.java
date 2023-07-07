69ebf97e120eb74556736720108ad1640cd07e37
DinuthDheeraka <dinuthdheeraka345@gmail.com>
2023-07-07 20:59:38 +0530

    added JdbcUserDetalManager configs

--- src/main/java/com/ceyentra/springboot/visitersmanager/config/security/LoginSecurityConfig.java
+++ src/main/java/com/ceyentra/springboot/visitersmanager/config/security/LoginSecurityConfig.java
@@ -4,8 +4,31 @@
  */
 package com.ceyentra.springboot.visitersmanager.config.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class LoginSecurityConfig {
+
+    @Bean
+    public UserDetailsManager userDetailsManager(DataSource dataSource) {
+
+        JdbcUserDetailsManager jdbcUserDetailsManager = new JdbcUserDetailsManager(dataSource);
+
+        //query to retrieve user by user name * regular sql
+        jdbcUserDetailsManager.setUsersByUsernameQuery(
+                "select user_name,pw,active from user where user_name=?"
+        );
+
+        //query to retrieve role by given user name * regular sql
+        jdbcUserDetailsManager.setAuthoritiesByUsernameQuery("" +
+                "select user_id,role from role where user_id=?"
+        );
+        return jdbcUserDetailsManager;
+    }
+
 }
