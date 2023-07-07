809c1cf57d282dd00574767eec07be42963c91e7
DinuthDheeraka <dinuthdheeraka345@gmail.com>
2023-07-07 16:59:02 +0530

    added project

---
+++ src/main/java/com/ceyentra/springboot/visitersmanager/VisitersmanagerApplication.java
@@ -0,0 +1,13 @@
+package com.ceyentra.springboot.visitersmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VisitersmanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VisitersmanagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ceyentra/springboot/visitersmanager/VisitersmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ceyentra.springboot.visitersmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VisitersmanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
