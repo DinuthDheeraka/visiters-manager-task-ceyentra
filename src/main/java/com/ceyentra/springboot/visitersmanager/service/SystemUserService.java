a1235d3937501b3481c25ce9d80a3b14decd53a6
DinuthDheeraka <dinuthdheeraka345@gmail.com>
2023-07-08 01:57:53 +0530

    updated SystemUserService

--- src/main/java/com/ceyentra/springboot/visitersmanager/service/SystemUserService.java
+++ src/main/java/com/ceyentra/springboot/visitersmanager/service/SystemUserService.java
@@ -1,5 +1,18 @@
 package com.ceyentra.springboot.visitersmanager.service;
 
+import com.ceyentra.springboot.visitersmanager.dto.SystemUserDTO;
+
+import java.util.List;
+
 public interface SystemUserService {
 
+    SystemUserDTO saveSystemUser();
+
+    SystemUserDTO updateSystemUser();
+
+    SystemUserDTO deleteSystemUserById(int id);
+
+    SystemUserDTO readSystemUserById(int id);
+
+    List<SystemUserDTO> readAllSystemUsers();
 }
