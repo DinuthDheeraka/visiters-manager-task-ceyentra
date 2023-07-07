b08bfc364a21deb26d4ba7e8f4d0d99b8f9e2f70
DinuthDheeraka <dinuthdheeraka345@gmail.com>
2023-07-07 20:51:40 +0530

    added security configs classes

---
+++ src/main/java/com/ceyentra/springboot/visitersmanager/config/security/AuthorizationSecurityConfig.java
@@ -0,0 +1,11 @@
+/**
+ * @author :  Dinuth Dheeraka
+ * Created : 7/7/2023 8:48 PM
+ */
+package com.ceyentra.springboot.visitersmanager.config.security;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AuthorizationSecurityConfig {
+}
