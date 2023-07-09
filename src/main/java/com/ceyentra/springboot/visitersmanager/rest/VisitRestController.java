890c5b05de6fc2e34df8a78af5c3ff35e293e5c5
DinuthDheeraka <dinuthdheeraka345@gmail.com>
2023-07-09 21:50:47 +0530

    added visitRestController

---
+++ src/main/java/com/ceyentra/springboot/visitersmanager/rest/VisitRestController.java
@@ -0,0 +1,15 @@
+/**
+ * @author :  Dinuth Dheeraka
+ * Created : 7/9/2023 9:49 PM
+ */
+package com.ceyentra.springboot.visitersmanager.rest;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/visit")
+public class VisitRestController {
+
+
+}
