ba22011e1400986bcea6a749bdbc4c54dd9cb752
DinuthDheeraka <dinuthdheeraka345@gmail.com>
2023-07-11 12:57:15 +0530

    added VisitorErrorResponse

---
+++ src/main/java/com/ceyentra/springboot/visitersmanager/exceptions/response/VisitorErrorResponse.java
@@ -0,0 +1,21 @@
+/**
+ * @author :  Dinuth Dheeraka
+ * Created : 7/11/2023 12:55 PM
+ */
+package com.ceyentra.springboot.visitersmanager.exceptions.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class VisitorErrorResponse {
+
+    private int status;
+
+    private String message;
+
+    private long timeStamp;
+}
