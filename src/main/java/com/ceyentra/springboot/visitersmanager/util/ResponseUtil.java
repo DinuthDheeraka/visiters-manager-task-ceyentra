376777735961f02d3bfacb86eb7f5fbeb549b4b9
DinuthDheeraka <dinuthdheeraka345@gmail.com>
2023-07-11 13:39:09 +0530

    added ResponseUtil

--- src/main/java/com/ceyentra/springboot/visitersmanager/util/ResponseUtil.java
+++ src/main/java/com/ceyentra/springboot/visitersmanager/util/ResponseUtil.java
@@ -4,5 +4,18 @@
  */
 package com.ceyentra.springboot.visitersmanager.util;
 
-public class ResponseUtil {
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ResponseUtil <T>{
+
+    private int status;
+
+    private String message;
+
+    private T data;
 }
