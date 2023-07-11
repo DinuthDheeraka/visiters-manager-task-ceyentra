eca1fbf38d84599cafc5bc2a23adbc64f2af1038
DinuthDheeraka <dinuthdheeraka345@gmail.com>
2023-07-11 14:26:01 +0530

    refactored getAllVisitors

--- src/main/java/com/ceyentra/springboot/visitersmanager/rest/VisitorRestController.java
+++ src/main/java/com/ceyentra/springboot/visitersmanager/rest/VisitorRestController.java
@@ -6,11 +6,16 @@ package com.ceyentra.springboot.visitersmanager.rest;
 
 import com.ceyentra.springboot.visitersmanager.dto.entity.VisitDTO;
 import com.ceyentra.springboot.visitersmanager.dto.entity.VisitorDTO;
+import com.ceyentra.springboot.visitersmanager.exceptions.VisitorNotFoundException;
 import com.ceyentra.springboot.visitersmanager.service.VisitorService;
+import com.ceyentra.springboot.visitersmanager.util.ResponseUtil;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/visitor")
@@ -24,8 +29,20 @@ public class VisitorRestController {
     }
 
     @GetMapping
-    public List<VisitorDTO> getAllVisitors() {
-        return visitorService.readAllVisitors();
+    public ResponseEntity<ResponseUtil<List<VisitorDTO>>> getAllVisitors() {
+
+        Optional<List<VisitorDTO>> optionalVisitorDTOS = Optional.ofNullable(visitorService.readAllVisitors());
+
+        if(optionalVisitorDTOS.isEmpty()){
+            throw new VisitorNotFoundException("Unable to find visitors");
+        }
+
+        return new ResponseEntity(new ResponseUtil(
+                        HttpStatus.OK.value(), "success" ,optionalVisitorDTOS.get()
+                ),
+                HttpStatus.OK
+        );
+
     }
 
     @GetMapping("/{id}")
