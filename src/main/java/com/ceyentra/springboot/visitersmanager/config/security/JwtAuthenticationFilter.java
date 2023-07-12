b66a63f713f6abc700bd7a6dd43626c774c4d681
DinuthDheeraka <dinuthdheeraka345@gmail.com>
2023-07-12 13:26:54 +0530

    added JwtAuthnticationFilter

---
+++ src/main/java/com/ceyentra/springboot/visitersmanager/config/security/JwtAuthenticationFilter.java
@@ -0,0 +1,21 @@
+/**
+ * @author :  Dinuth Dheeraka
+ * Created : 7/12/2023 1:22 PM
+ */
+package com.ceyentra.springboot.visitersmanager.config.security;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
+    }
+}
